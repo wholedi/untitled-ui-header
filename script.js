cc4bc7faff0aa0d952dcb195f16e0d4e825a7f84
Taras Shevchenko <131402561+wholedi@users.noreply.github.com>
2023-06-16 00:00:11 +0300

    header adaptive part three.

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-const menuBtn = document.querySelector('#nav-icon2');
-const menuList = document.querySelector('.menu__list');
+const menuBtn = document.querySelector('#nav-icon2')
+const menuList = document.querySelector('.header__menu')
 
 menuBtn.addEventListener('click', () => {
-    menuBtn.classList.toggle('open');
-   //  menuList.classList.toggle('active');
-});
+    menuBtn.classList.toggle('open')
+    menuList.classList.toggle('active')
+})
+
+const itemDropdown = document.querySelectorAll('.menu__item.menu-item_dropdown')
+
+itemDropdown.addEventListener('click', () => {
+    itemDropdown.classList.toggle('active')
+
+})
+
+
 
